3cb499c328c8210b3eaa773022853bed56470ff1
Jonathan <jonathancouto3030@hotmail.com>
2024-04-18 09:22:38 -0300

    Remoção de func que não precisa pro projeto

--- src/js/form.js
+++
@@ -1,8 +0,0 @@
-function submitForm(){
-    const btnForm = document.getElementById('submitForm');
-    btnForm.addEventListener('click', () => {
-        console.log('Enviou');
-    })
-}
-
-submitForm();
